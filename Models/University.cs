887f009cc33d50f6dec6c736c88edc35b324c1d6
Yosia Kehat Driesa <kehatdriesayosia@gmail.com>
2023-05-17 10:26:22 +0700

    Initial commit

---
+++ Models/University.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class University
+    {
+        public Guid Guid { get; set; }
+        public string Code { get; set; }
+        public string name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
